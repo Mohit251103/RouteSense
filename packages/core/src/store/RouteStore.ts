7f21c36e67dc8f4a9af8d84f4e77507feaaddd5a
Mohit Negi <mnegi1433@gmail.com>
2025-11-28 23:24:06 +0530

    removed qdrant, switching to mongodb vector search

--- packages/core/src/store/RouteStore.ts
+++ packages/core/src/store/RouteStore.ts
@@ -1,48 +1,54 @@
-// Route Store Module
-// This module provides functionality to store and manage classified routes.
+// // Route Store Module
+// // This module provides functionality to store and manage classified routes.
 
-import { createCollectionIfNotExists, qdrant } from "../db/qdrant";
-import { embed } from "../utils/embed";
+// // import { createCollectionIfNotExists, qdrant } from "../db/mongo";
+// import { embed } from "../utils/embed";
 
-class RouteStore {
+// class RouteStore {
 
-    public async addToSemanticStore(route: string, apiKey: string) {
-        // Implementation to add route to semantic store
-        const vector = await embed(route);
+//     public async addToSemanticStore(route: string, apiKey: string) {
+//         // Implementation to add route to semantic store
+//         const vector = await embed(route);
     
-        // Store embeddings in the semantic store associated with the apiKey
-        await createCollectionIfNotExists('routes');
-        await qdrant.upsert('routes', {
-            points: [
-                {
-                    id: `${apiKey}-${Date.now()}`,
-                    vector: vector,
-                    payload: { route: route, apiKey: apiKey }
-                }
-            ]
-        })
-    }
+//         // Store embeddings in the semantic store associated with the apiKey
+//         await createCollectionIfNotExists('routes');
+//         try {
+//             await qdrant.upsert('routes', {
+//                 wait: true,
+//                 points: [
+//                     {
+//                         id: `${apiKey}-${Date.now()}`,
+//                         vector,
+//                         payload: { route: route, apiKey: apiKey }
+//                     }
+//                 ]
+//             })
+//         } catch (error) {
+//             console.debug((error as Error).stack);
+//         }
+//     }
 
-    public async addToTemplateStore(route: string, apiKey: string) {
-        // Implementation to add route to template store
-    }
+//     public async addToTemplateStore(route: string, apiKey: string) {
+//         // Implementation to add route to template store
+//     }
 
-    public async addToIgnoreStore(route: string) {
-        // Implementation to add route to ignore store
-    }
-};
+//     public async addToIgnoreStore(route: string) {
+//         // Implementation to add route to ignore store
+//     }
+// };
 
-let persistentRouteStore: RouteStore | null = null;
+// let persistentRouteStore: RouteStore | null = null;
 
-// Factory function to create a persistent RouteStore instance
-export function createRouteStore(): RouteStore {
-    if (!persistentRouteStore) {
-        persistentRouteStore = new RouteStore();
-    }
-    return persistentRouteStore;
-}
+// // Factory function to create a persistent RouteStore instance
+// export function createRouteStore(): RouteStore {
+//     if (!persistentRouteStore) {
+//         persistentRouteStore = new RouteStore();
+//     }
+//     return persistentRouteStore;
+// }
 
-// Function to get the existing persistent RouteStore instance
-export function getRouteStore(): RouteStore | null {
-    return persistentRouteStore;
-}
+// // Function to get the existing persistent RouteStore instance
+// export function getRouteStore(): RouteStore | null {
+//     return persistentRouteStore;
+// }
+// 
